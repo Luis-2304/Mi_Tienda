77ac54f4368d5707fe8d779795d1e4172ccb27f1
Luis-2304 <lahr48390@gmail.com>
2024-05-13 23:17:16 -0600

    first commit

---
+++ src/use-cases/queries/conexion.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5uQNXebaz3OCaDI6PoYpHKWPCGwECQCg",
+  authDomain: "mi-tienda-d11bd.firebaseapp.com",
+  projectId: "mi-tienda-d11bd",
+  storageBucket: "mi-tienda-d11bd.appspot.com",
+  messagingSenderId: "1033887773364",
+  appId: "1:1033887773364:web:81be79cf747e495e83dde1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
